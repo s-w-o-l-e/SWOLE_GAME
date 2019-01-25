9dd669f41636328d3233372545cbb367b53ba5cd
Gil9 <Gilzm99@gmail.com>
2019-01-25 16:21:42 +0200

    Added basic player

---
+++ S W O L E/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offest = transform.position - player.transform.position;
+    }
+
+    // Guarentee to run after all items have been processed in update
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offest;
+    }
+}
