28d055357f9df5929b659f32abe2e591fd43c9e6
Gil9 <Gilzm99@gmail.com>
2019-01-26 21:26:37 +0200

    Removed flying ability from Elli and added base enemies

--- S W O L E/Assets/Scripts/PlayerController.cs
+++ S W O L E/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,6 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     public float speed;
     public float rotationSpeed;
-    
-    private float xForce;
-    private float zForce;
-    private Vector3 force;
-    private float pitch = 0.0F;
     private float yaw = 0.0F;
 
     void Start()
@@ -21,6 +16,9 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        float y = transform.localPosition.y;
+        var ogRotation = rb.transform.localEulerAngles;
+
         // move relative to blue arrow
         if (Input.GetKey(KeyCode.W))
         {
@@ -39,15 +37,12 @@ public class PlayerController : MonoBehaviour
             transform.localPosition += transform.right * speed * Time.deltaTime;
         }
 
+        transform.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
+
         //pitch -= Input.GetAxis("Mouse Y");
         yaw += Input.GetAxis("Mouse X") * rotationSpeed;
 
-        force = new Vector3(xForce, 0.0F, zForce);
-
         // rotate object to face mouse direction
-        rb.transform.localEulerAngles = new Vector3(pitch, yaw, 0.0F);
-
-        // move object in facing direction relative to local (AddRelative) not world (AddForce) coordinates
-        rb.AddRelativeForce(force);
+        rb.transform.localEulerAngles = new Vector3(0.0F, yaw, 0.0F);
     }
 }
