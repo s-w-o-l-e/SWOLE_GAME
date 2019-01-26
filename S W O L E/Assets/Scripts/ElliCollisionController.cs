c24b7013280383ebc42105c1ed4b9a4547d13ae9
Dean Kevorkian <deankevor@gmail.com>
2019-01-26 20:51:22 +0200

    WOOOOOOO Changed script so objects we collide with now fall through Elli!!!!

--- S W O L E/Assets/Scripts/ElliCollisionController.cs
+++ S W O L E/Assets/Scripts/ElliCollisionController.cs
@@ -20,19 +20,42 @@ public class ElliCollisionController : MonoBehaviour
 
     void OnTriggerEnter(Collider collider)
     {
+        Debug.Log("mamazav");
+        var collidedGameObject = collider.gameObject;
+        if (destructionWhitelist.Contains(collidedGameObject.tag)) return;
+
+        // If there is no "destroyedObject" defined it means we're in the "destroyed" version of our object,
+        // and not the original one. Hence, we don't re-spawn a destroyed object infinitely...
+        else
+        {
+            collidedGameObject.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+
+    /*void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("mamazav");
         var collidedGameObject = collider.gameObject;
         if (destructionWhitelist.Contains(collidedGameObject.tag)) return;
 
         // Try obtaining the "destroyedObject" part of the object we're going to destroy.
         // This is necessary for visual effects - everytime we are about to destroy an object,
         // we want to instantiate a "destroyedObject" that takes care of the animations.
-        var destroyedObject = collidedGameObject.GetComponent<DestructibleObject>().destroyedObject;
+        var destroyedObject = collidedGameObject.GetComponent<DestructibleObject>();
+
+        // Destroy(collider.gameObject);
 
         if (destroyedObject != null)
         {
-            Instantiate(destroyedObject, collidedGameObject.transform.position, collidedGameObject.transform.rotation);
+            var actualDestroyedObject = destroyedObject.destroyedObject;
+            Destroy(collider.gameObject);
+            Instantiate(actualDestroyedObject, collidedGameObject.transform.position, collidedGameObject.transform.rotation);
         }
-
-        Destroy(collider.gameObject);
-    }
+        // If there is no "destroyedObject" defined it means we're in the "destroyed" version of our object,
+        // and not the original one. Hence, we don't re-spawn a destroyed object infinitely...
+        else
+        {
+            collidedGameObject.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }*/
 }
