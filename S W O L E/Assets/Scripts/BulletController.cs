367894e3978a7266f9db63a6824d718f115e21a7
Gil9 <Gilzm99@gmail.com>
2019-02-20 15:39:28 +0200

    Added on bullet hit

---
+++ S W O L E/Assets/Scripts/BulletController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public AudioSource m_ExplosionAudio;                // Reference to the audio that will play on explosion.
+    public float m_MaxLifeTime = 5f;                    // The time in seconds before the shell is removed.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // If it isn't destroyed by then, destroy the shell after it's lifetime.
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //Rigidbody target = col.GetComponent<Rigidbody>();
+            // Reduce health from player
+            
+            // Play the explosion sound effect.
+            // Should be on the player
+            m_ExplosionAudio.Play();
+
+            // Destroy the shell.
+            Destroy(gameObject);
+        }
+    }
+}
