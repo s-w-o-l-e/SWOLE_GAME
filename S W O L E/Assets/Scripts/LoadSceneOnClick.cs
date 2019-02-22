96624c6ab3253022a4eb3ae043d9520e03f1df5e
Gil9 <Gilzm99@gmail.com>
2019-02-22 12:42:09 +0200

    Main Menu

---
+++ S W O L E/Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+    public float menuFadeTime = .5f;
+    public Color sceneChangeFadeColor = Color.black;
+
+    public void LoadByIndex(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
