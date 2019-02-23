f1e9f7fe4da618d308320cde6bd5429dc81e4412
Gil9 <Gilzm99@gmail.com>
2019-02-23 19:18:37 +0200

    Border scales with eli's size

--- S W O L E/Assets/Scripts/PlayerController.cs
+++ S W O L E/Assets/Scripts/PlayerController.cs
@@ -50,21 +50,21 @@ public class PlayerController : MonoBehaviour
             rb.transform.localEulerAngles = new Vector3(0.0F, yaw, 0.0F);
         }
 
-        if (transform.localPosition.z <= -10)
+        if (transform.localPosition.z - transform.localScale.z / 2 <= -17)
         {
-            transform.localPosition = new Vector3(transform.position.x, transform.position.y, -10);
+            transform.localPosition = new Vector3(transform.position.x, transform.position.y, -17 + transform.localScale.z/2);
         }
-        if (transform.localPosition.z >= 49)
+        if (transform.localPosition.z + transform.localScale.z / 2 >= 56)
         {
-            transform.localPosition = new Vector3(transform.position.x, transform.position.y, 49);
+            transform.localPosition = new Vector3(transform.position.x, transform.position.y, 56 - transform.localScale.z / 2);
         }
-        if (transform.localPosition.x <= -23)
+        if (transform.localPosition.x - transform.localScale.x/2 <= -28)
         {
-            transform.localPosition = new Vector3(-23, transform.position.y, transform.position.z);
+            transform.localPosition = new Vector3(-28 + transform.localScale.x / 2, transform.position.y, transform.position.z);
         }
-        if (transform.localPosition.x >= 47)
+        if (transform.localPosition.x + transform.localScale.x / 2 >= 52)
         {
-            transform.localPosition = new Vector3(47, transform.position.y, transform.position.z);
+            transform.localPosition = new Vector3(52 - transform.localScale.x / 2, transform.position.y, transform.position.z);
         }
     }
 
