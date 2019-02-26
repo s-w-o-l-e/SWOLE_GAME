51f66c72b005c78ab9331a6a52f521805e882ba6
Gil9 <Gilzm99@gmail.com>
2019-02-26 04:59:16 +0200

    in-game tutorial

---
+++ S W O L E/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+    private bool bSwallowedItem;
+    private bool bSwallowedZombie;
+    public GameObject TutorialText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bSwallowedItem = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bSwallowedItem)
+        {
+            TutorialText.GetComponent<UnityEngine.UI.Text>().text = "Find a smaller zombie to swallow!";
+            TutorialText.GetComponent<UnityEngine.UI.Text>().color = Color.blue;
+        }
+        if (bSwallowedZombie)
+        {
+            //TutorialText.GetComponent<UnityEngine.UI.Text>().enabled = false;
+            TutorialText.GetComponent<UnityEngine.UI.Text>().color = Color.green;
+            TutorialText.GetComponent<UnityEngine.UI.Text>().text = "Great! Swallow all the zombies to advance to the next level!";
+        }
+    }
+
+    void OnEnable()
+    {
+        EventManagerController.StartListening("SwallowMapItem", () =>
+        {
+            bSwallowedItem = true;
+        });
+        EventManagerController.StartListening("SwallowZumbi", () =>
+        {
+            bSwallowedZombie = true;
+        });
+    }
+}
